86d7339a97b69b58cde75576a1f4e396b543e500
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-09-06 20:47:14 +0530

    added the middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,2 +1,4 @@
 module.exports = {
+    AuthRequestMiddleWares: require('./user-middlewares')
+
 }
--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const {UserController}=require('../../controllers');
+const { AuthRequestMiddleWares } = require('../../middlewares');
 
 const router = express.Router();
 
           router.post('/signup',
+                          AuthRequestMiddleWares.ValidateAuthRequest,
                           UserController.signup)
           router.post('/signin',
+                          AuthRequestMiddleWares.ValidateAuthRequest,
                           UserController.signin)
 
 
